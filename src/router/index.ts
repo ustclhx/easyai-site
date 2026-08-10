3f38e40361a4bcf593c9f0a87984bf90261619a8
Haixin Li <28524709+ustclhx@users.noreply.github.com>
2026-08-10 20:36:36 +0800

    Configure GitHub Pages deployment

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   scrollBehavior: () => ({ top: 0, behavior: "smooth" }),
   routes: [
     { path: "/", name: "home", component: () => import("@/views/HomeView.vue") },
